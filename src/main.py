d58dd520fd4fcb85c99803229c22c5d67cb34b96
Paulius <paulius031@gmail.com>
2025-09-24 17:11:59 +0300

    minimalus fastAPI

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="fastapi-docker")
+
+
+@app.get("/labas")
+def labas():
+    return {"message": "labas"}
+
+
+@app.get("/")
+def home():
+    return "Namai"
+
+
